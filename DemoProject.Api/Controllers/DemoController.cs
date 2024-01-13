f2eef580983824aa47fa4a7ae786289e2f316543
Matheus Gonçalves Rodrigues <matheus.rodrigues@opah.com.br>
2024-01-13 16:55:13 -0300

    Add project files.

---
+++ DemoProject.Api/Controllers/DemoController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DemoProject.Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class DemoController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok("Ok");
+    }
+}
